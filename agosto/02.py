ebd9c6c571a2679f3f0aaa78f087304caff7a52f
Joaquimfl8 <256233831+JoaFL@users.noreply.github.com>
2026-08-18 15:47:21 -0300

    init

---
+++ agosto/02.py
@@ -0,0 +1,24 @@
+import pygame
+
+LARGURA = 800
+ALTURA = 450
+AZUL_ESCURO = (20, 35, 70)
+BRANCO = (255, 255, 255)
+
+pygame.init()
+tela = pygame.display.set_mode((LARGURA, ALTURA))
+pygame.display.set_caption("Coordenadas e cores")
+
+rodando = True
+while rodando:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            rodando = False
+            
+    tela.fill(AZUL_ESCURO)
+    
+    pygame.draw.circle(tela, BRANCO, (0, 0), 8)
+    pygame.draw.circle(tela, BRANCO, (LARGURA // 2, ALTURA // 2), 8)
+    pygame.display.flip()
+
+pygame.quit()
